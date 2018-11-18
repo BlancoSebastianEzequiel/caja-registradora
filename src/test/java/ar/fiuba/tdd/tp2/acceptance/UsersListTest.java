aaa91149a02d5e698848af45d69d24e18c5f80b0
sebastian <sebastian.e.blanco@gmail.com>
2018-11-18 17:16:25 -0300

    test not existance users

---
+++ src/test/java/ar/fiuba/tdd/tp2/acceptance/UsersListTest.java
@@ -0,0 +1,27 @@
+package ar.fiuba.tdd.tp2.acceptance;
+
+import ar.fiuba.tdd.tp2.Users;
+import org.json.simple.parser.ParseException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class UsersListTest {
+    Users users;
+
+    @Before
+    public void setUp() throws IOException, ParseException {
+        this.users = new Users("users.json");
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test(expected = ar.fiuba.tdd.tp2.UserDoesNotExist.class)
+    public void notExistanceUserShouldRaiseException() {
+        this.users.get("doesNotExists");
+    }
+}
