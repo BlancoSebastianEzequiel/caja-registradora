59b00d055359eabe0fdab216ecb90ecffdbe817e
sebastian <sebastian.e.blanco@gmail.com>
2018-11-18 22:31:36 -0300

    use TicketControl singleton to log data

--- src/main/java/ar/fiuba/tdd/tp2/CashRegister.java
+++ src/main/java/ar/fiuba/tdd/tp2/CashRegister.java
@@ -11,12 +11,14 @@ public class CashRegister implements CashRegisterInterface {
     private JsonConverter offers;
     private JsonConverter rules;
     private User user;
+    private Integer totalCash;
 	
     public CashRegister(String usersFile, String offersFile, String rulesFile) throws IOException, ParseException {
         this.state = new Close();
         this.users = new Users(usersFile);
         this.offers = new JsonConverter(offersFile);
         this.rules = new JsonConverter(rulesFile);
+        this.totalCash = 0;
     }
     
     public void changeState(CashRegisterState newState) {
@@ -24,6 +26,7 @@ public class CashRegister implements CashRegisterInterface {
     }
 
     public void open(String username, String password) {
+        ControlTicket.getInstance().openCashRegister();
         this.state.open(this, this.users.getUser(username, password));
     }
 
@@ -34,7 +37,10 @@ public class CashRegister implements CashRegisterInterface {
     public Boolean isUserSignedIn() {
         return this.state.isUserSignedIn();
     }
+
     public void close(String username, String password) {
+        ControlTicket.getInstance().logCashRegisterTotalCash(this.totalCash);
+        ControlTicket.getInstance().closeCashRegister(this.user);
         this.state.close(this, this.users.getUser(username, password));
 
     }
@@ -48,6 +54,11 @@ public class CashRegister implements CashRegisterInterface {
     }
 
     public void initSale() {}
-    public void finishSale() {}
+
+    public void finishSale() {
+        // TODO: implementar issue #5 y #6 donde se crea la compra para obtener estos datos
+        ControlTicket.getInstance().logShipment(null, null, null);
+    }
+
     public void addItemToCurrentSale(String item) {}
 }
