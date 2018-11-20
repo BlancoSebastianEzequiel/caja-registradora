37ca1aaf19b8aedd80133e9a89a62833e3541c02
sebastian <sebastian.e.blanco@gmail.com>
2018-11-19 22:02:54 -0300

    add message to exceptions

--- src/main/java/ar/fiuba/tdd/tp2/exceptions/ProductDoesNotExist.java
+++ src/main/java/ar/fiuba/tdd/tp2/exceptions/ProductDoesNotExist.java
@@ -2,4 +2,8 @@ package ar.fiuba.tdd.tp2.exceptions;
 
 public class ProductDoesNotExist  extends RuntimeException {
     private static final long serialVersionUID = 1L;
+
+    public String print() {
+        return "Product does not exist";
+    }
 }
