de749a0d4e4ed64ecdc465c461c0eef3a611a797
sebastian <sebastian.e.blanco@gmail.com>
2018-11-21 00:40:46 -0300

    add message to all exceptions

--- src/main/java/ar/fiuba/tdd/tp2/exceptions/UserDoesNotExist.java
+++ src/main/java/ar/fiuba/tdd/tp2/exceptions/UserDoesNotExist.java
@@ -3,7 +3,8 @@ package ar.fiuba.tdd.tp2.exceptions;
 public class UserDoesNotExist extends RuntimeException {
     private static final long serialVersionUID = 1L;
 
-    public String print() {
+    @Override
+    public String getMessage() {
         return "User does not exist";
     }
 }
