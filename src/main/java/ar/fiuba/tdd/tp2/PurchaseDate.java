e889de4202fd621fbf5ef019663a9ef3a368e1b1
sebastian <sebastian.e.blanco@gmail.com>
2018-11-21 16:06:03 -0300

    fix atributes type

--- src/main/java/ar/fiuba/tdd/tp2/PurchaseDate.java
+++ src/main/java/ar/fiuba/tdd/tp2/PurchaseDate.java
@@ -6,11 +6,11 @@ import java.util.Date;
 public class PurchaseDate {
 
     private Date date;
-    private String year;
+    private Integer year;
     private String month;
-    private int dayNumber;
+    private Integer dayNumber;
     private String weekDay;
-    private int weekNumber;
+    private Integer weekNumber;
     enum Day {
         SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
     }
@@ -22,26 +22,26 @@ public class PurchaseDate {
         this.date = new Date();
         Calendar cal = Calendar.getInstance();
         cal.setTime(date);
-        this.year = ((Integer) cal.get(Calendar.YEAR)).toString();
+        this.year = cal.get(Calendar.YEAR);
         this.month = Month.values()[cal.get(Calendar.MONTH)].toString();
         this.dayNumber = cal.get(Calendar.DAY_OF_MONTH);
         this.weekDay = Day.values()[cal.get(Calendar.DAY_OF_WEEK)].toString();
         this.weekNumber = cal.get(Calendar.WEEK_OF_MONTH);
     }
 
-    public String getYear() {
+    public Integer getYear() {
         return this.year;
     }
     public String getMonth() {
         return this.month;
     }
-    public int getDayNumber() {
+    public Integer getDayNumber() {
         return this.dayNumber;
     }
     public String getWeekDay() {
         return this.weekDay;
     }
-    public int getWeekNumber() {
+    public Integer getWeekNumber() {
         return this.weekNumber;
     }
 }
